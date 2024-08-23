f886ec5d45672ed8e220b2672beb6889bbbec1b1
LouisCuenot <70881127+LouisCuenot@users.noreply.github.com>
2024-08-23 10:34:34 +0200

    New Schema Type

--- app/models/Medal.ts
+++ app/models/Medal.ts
@@ -8,10 +8,44 @@ export interface MedalDataType extends Document {
 
 const medalSchema:Schema<MedalDataType> = new Schema<MedalDataType>({
     medalID:{
+        type:String,
         required:true
     },
-    medal:{
-        required:true
+    medal: {
+        metal: {
+            type: String,
+            enum: ['gold', 'silver', 'bronce'],
+            required: true
+        },
+        collier: {
+            type: String,
+            enum: ['green', 'blue', 'pink', 'cyan', 'grey', 'red', 'yellow', 'purple', 'white', 'black'],
+            required: true
+        },
+        contours: {
+            type: String,
+            enum: ['stars', 'points', 'lauriers', 'tripleLines', 'verticalLines'],
+            required: true
+        },
+        icon: {
+            type: String,
+            enum: ['sport', 'trash', 'cuisine', 'menage', 'chrono', 'toilette', 'course'],
+            required: true
+        },
+        content: {
+            title: {
+                type: String,
+                required: true
+            },
+            mission: {
+                type: String,
+                required: true
+            },
+            date: {
+                type: String,
+                required: true
+            }
+        }
     }
 })
 
